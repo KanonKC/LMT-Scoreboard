f59f0065779befdd91589678807a9f1132027649
KanonKC <kanonkce@gmail.com>
2022-11-17 21:07:22 +0700

    Add medal icon

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Card, Col, Row } from "reactstrap";
 
-const ORDER = ['st','nd','rd','th']
+const ORDER = ['st','nd','rd']
+const MEDAL = ['gold','silver','bronze']
 
 const ScoreCard = ({ team,score,order,color }) => {
 
     const [orderSymbol,setorderSymbol] = useState("th")
+    const [medalIcon,setmedalIcon] = useState(null)
 
     useEffect(()=>{
-        if(order <= 4){
+        if(order < 4){
+            setmedalIcon(MEDAL[order-1])
             setorderSymbol(ORDER[order-1])
         }
     },[order])
@@ -20,6 +23,9 @@ const ScoreCard = ({ team,score,order,color }) => {
             <Row>
                 <Col xs={1}>
                     <h1 className="font-mono">{order}<sup>{orderSymbol}</sup></h1>
+                </Col>
+                <Col xs={1}>
+                    {medalIcon && <img width={50} src={require(`../imgs/${medalIcon}-medal.svg`)} alt=""/>}
                 </Col>
                 <Col>
                     <h1>{team}</h1>
