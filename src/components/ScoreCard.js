44ba8673c59a5014c653c97332b04066f8733954
KanonKC <kanonkce@gmail.com>
2022-11-17 19:47:24 +0700

    Card order, Refresh counter

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Col, Row } from "reactstrap";
 
+const ORDER = ['st','nd','rd','th']
+
 const ScoreCard = ({ team,score,order,color }) => {
+
+    const [orderSymbol,setorderSymbol] = useState("th")
+
+    useEffect(()=>{
+        if(order <= 4){
+            setorderSymbol(ORDER[order-1])
+        }
+    },[order])
+
     return (
         <Card className="py-3 px-4" style={{
             backgroundColor: color
         }}>
             <Row>
+                <Col xs={1}>
+                    <h1 className="font-mono">{order}<sup>{orderSymbol}</sup></h1>
+                </Col>
                 <Col>
-                    <h1 >{order}. {team}</h1>
+                    <h1>{team}</h1>
                 </Col>
                 <Col>
                     <h1 className="text-right">{score}</h1>
--- src/views/Scoreboard.js
+++ src/views/Scoreboard.js
@@ -1,26 +1,47 @@
 import React, { useEffect, useState } from "react";
 import ScoreCard from "../components/ScoreCard";
 import { getScores } from "../services/score.service";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import { Col, Row } from "reactstrap";
 
 const Scoreboard = () => {
     const [scores, setscores] = useState([]);
+    const [key, setKey] = useState(0);
+
+    const handleComplete = (prevKey) => {
+        setKey((prevKey) => prevKey + 1);
+        getScores().then((response) => {
+          setscores(response.data.result);
+        });
+    };
 
     useEffect(() => {
-        setInterval(()=>{
         getScores().then((response) => {
             setscores(response.data.result);
         });
-        },1100)
     }, []);
 
     return (
         <div>
-            <h1 className="text-center my-10 text-6xl">SCOREBOARD</h1>
+            <h1 className="text-center mt-10 text-6xl">SCOREBOARD </h1>
+
+            <div className="my-10 countdown-timer">
+                <CountdownCircleTimer
+                    isPlaying
+                    size={50}
+                    duration={10}
+                    colors={["#004777"]}
+                    colorsTime={[10]}
+                    key={key}
+                    onComplete={(prevKey) => handleComplete(prevKey)}
+                />
+            </div>
+
             <div className="grid grid-rows-1 card-list gap-4">
                 {scores.map((item, index) => (
                     <ScoreCard
                         key={index}
-                        order={index+1}
+                        order={index + 1}
                         team={item.team}
                         score={item.score}
                         color={item.color}
